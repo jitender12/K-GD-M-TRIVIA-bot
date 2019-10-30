95fe7061f5d96bdcadb42a0d2b6ba3d6fe74daad
jitender12 <55946020+jitender12@users.noreply.github.com>
2019-10-30 19:08:03 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord=require('discord.js');
+const client=new Discord.Client();
+
+client.on('ready',()=> {
+    console.log('I am ready!'):
+});
+
+client.on('massage',massage=>
+   if (massage.content==='ping'){
+   massage.reply('pong');
+ });
+ 
+ // THIS MUST BE THIS WAY
+ client.login(NjM4MDg5MjA4NDU5OTUyMTI4.Xbauag.Tb3WQ5lS2ehwMUHIphxTTBg2Noc);
+
+    
